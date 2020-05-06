09331f4dac6831e6cfb3d6bca29c97d6bb36630e
Karol <koptyrakarol@gmail.com>
2020-05-06 15:49:39 +0200

    działający dispay planszy
    plansza zapełnianie kliknięciami

--- NonogramSolverGUI.py
+++ NonogramSolverGUI.py
@@ -7,32 +7,58 @@ class Grid:
     def __init__(self, rows, cols, width, height, window):
         self.rows = rows
         self.cols = cols
-        self.holes = [[Hole(x, y, width, height, window, rows, cols) for x in range(cols)] for y in range(rows)]
+        self.holes = [[Hole(x, y, width, height) for x in range(cols)] for y in range(rows)]
         self.width = width
         self.height = height
         self.window = window
+        self.sGap = 20
+        self.hintsHeight = 100
+        self.hintsWidth = 100
+        self.wGap = (self.width - self.hintsWidth - self.sGap) / self.cols
+        self.hGap = (self.height - self.hintsHeight - self.sGap) / self.rows
 
     def draw(self):
         # Rysowanie siatki
-        sGap = 20
-        wGap = (self.width - 100 - sGap) / self.cols
-        hGap = (self.height - 100 - sGap) / self.rows
         thickness = 2
 
         for i in range (self.cols + 1):
-            pygame.draw.line(self.window, (0,0,0), (100, 100 + i * wGap), (self.height - sGap, 100 + i * wGap), thickness)
+            pygame.draw.line(self.window, (0,0,0), (self.hintsWidth, self.hintsHeight + i * self.wGap), (self.height - self.sGap, self.hintsHeight + i * self.wGap), thickness)
 
         for i in range (self.rows + 1):
-            pygame.draw.line(self.window, (0,0,0), (100 + i * hGap, 100), (100 + i * hGap, self.width - sGap), thickness)
+            pygame.draw.line(self.window, (0,0,0), (self.hintsWidth + i * self.hGap, self.hintsHeight), (self.hintsWidth + i * self.hGap, self.width - self.sGap), thickness)
         
         # Rysowanie dziur
         for x in range(self.rows):
             for y in range(self.cols):
-                self.holes[x][y].draw(self.window)
+                self.holes[x][y].draw(self.window, self.rows, self.cols, self.hintsWidth, self.hintsHeight, self.sGap)
 
     def refresh(self):
         self.window.fill((255,255,255))
         self.draw()
+
+    def click(self, mousePosition):
+        (x, y) = mousePosition
+
+        if x < self.hintsWidth or y < self.hintsHeight:
+            return
+
+        if x > self.width - self.sGap or y > self.height - self.sGap:
+            return
+
+        (x, y) = (x - self.hintsWidth, y - self.hintsHeight)
+
+        col = int(x // self.wGap)
+        row = int(y // self.hGap)
+
+        hole = self.getHole(row, col)
+        hole.inc()
+
+    def getHole(self, row, col):
+        for holesList in self.holes:
+            for h in holesList:
+                if h.row == row and h.col == col:
+                    return h
+        return None
 
 
 # Hint do pojedynczej wskazówki
@@ -42,37 +68,52 @@ class Hint:
 # Hole do pojedynczej dziury
 class Hole:
 
-    def __init__(self, row, col, width, height, window, rows, cols):
+    def __init__(self, row, col, width, height):
         self.value = 0
         self.row = row
         self.col = col
         self.width = width
         self.height = height
-        self.window = window
-        self.rows = rows
-        self.cols = cols
     
-    def draw(self, window):
+    def draw(self, window, rows, cols, hW, hH, sGap):
 
         if self.value % 3 == 0:
             # Brak zapełnienia
             pass
         elif self.value % 3 == 1:
             # Zapełnienie czarnym kolorem
-            sGap = 20
-            wGap = (self.width - 100 - sGap) / self.cols
-            hGap = (self.height - 100 - sGap) / self.rows
-            pygame.draw.rect(self.window, (0, 0, 0), (100 + self.col * hGap, 100 + self.row * wGap, hGap, wGap), 0)
-            pass
+            wGap = (self.width - hW - sGap) / cols
+            hGap = (self.height - hH - sGap) / rows
+            pygame.draw.rect(window, (0, 0, 0), (hW + self.col * hGap + 3, hH + self.row * wGap + 3, hGap - 4, wGap - 4), 0)
         else: 
-            # Zapełnienie X
-            pass
+            # Zapełnienie X / szarym
+            wGap = (self.width - hW - sGap) / cols
+            hGap = (self.height - hH - sGap) / rows
+            pygame.draw.rect(window, (200, 200, 200), (hW + self.col * hGap + 3, hH + self.row * wGap + 3, hGap - 4, wGap - 4), 0)
+            #pygame.draw.line(window, (0,0,0), (hW + self.col * hGap, hH + self.row * wGap), (hW + (self.col + 1) * hGap, hH + (self.row + 1) * wGap), 3)
+            #pygame.draw.line(window, (0,0,0), (hW + (self.col + 1) * hGap, hH + self.row * wGap), (hW + self.col * hGap, hH + (self.row + 1) * wGap), 3)
+    
+    def inc(self):
+        self.value += 1
     
 if __name__ == "__main__":
-    window = pygame.display.set_mode((600,600))
+    window = pygame.display.set_mode((600, 600))
     pygame.display.set_caption("NonogramSolver")
-    nonogramBoard = Grid(3, 3, 600, 600, window)
-    while True:
+    nonogramBoard = Grid(15, 15, 600, 600, window)
+    run = True
+    
+    while run:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                mousePosition = pygame.mouse.get_pos()
+                print(mousePosition)
+                nonogramBoard.click(mousePosition)
+
         nonogramBoard.refresh()
         pygame.display.update()
+
     pygame.quit()
