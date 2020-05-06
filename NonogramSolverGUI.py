fb1ff4d724462f0615bde93f5e8c221192d29975
Karol <koptyrakarol@gmail.com>
2020-05-06 14:22:53 +0200

    rysowanie dziur v1
    rysuje poprawnie tak jak chcemy ale crashuje(do zrobienia)

--- NonogramSolverGUI.py
+++ NonogramSolverGUI.py
@@ -7,16 +7,28 @@ class Grid:
     def __init__(self, rows, cols, width, height, window):
         self.rows = rows
         self.cols = cols
-        self.holes = [[Hole(x, y, width, height) for x in range(cols)] for y in range(rows)]
+        self.holes = [[Hole(x, y, width, height, window, rows, cols) for x in range(cols)] for y in range(rows)]
         self.width = width
         self.height = height
         self.window = window
 
     def draw(self):
         # Rysowanie siatki
-            
+        sGap = 20
+        wGap = (self.width - 100 - sGap) / self.cols
+        hGap = (self.height - 100 - sGap) / self.rows
+        thickness = 2
+
+        for i in range (self.cols + 1):
+            pygame.draw.line(self.window, (0,0,0), (100, 100 + i * wGap), (self.height - sGap, 100 + i * wGap), thickness)
+
+        for i in range (self.rows + 1):
+            pygame.draw.line(self.window, (0,0,0), (100 + i * hGap, 100), (100 + i * hGap, self.width - sGap), thickness)
+        
         # Rysowanie dziur
-        pass
+        for x in range(self.rows):
+            for y in range(self.cols):
+                self.holes[x][y].draw(self.window)
 
     def refresh(self):
         self.window.fill((255,255,255))
@@ -30,12 +42,15 @@ class Hint:
 # Hole do pojedynczej dziury
 class Hole:
 
-    def __init__(self, row, col, width, height):
+    def __init__(self, row, col, width, height, window, rows, cols):
         self.value = 0
         self.row = row
         self.col = col
         self.width = width
         self.height = height
+        self.window = window
+        self.rows = rows
+        self.cols = cols
     
     def draw(self, window):
 
@@ -44,15 +59,20 @@ class Hole:
             pass
         elif self.value % 3 == 1:
             # Zapełnienie czarnym kolorem
+            sGap = 20
+            wGap = (self.width - 100 - sGap) / self.cols
+            hGap = (self.height - 100 - sGap) / self.rows
+            pygame.draw.rect(self.window, (0, 0, 0), (100 + self.col * hGap, 100 + self.row * wGap, hGap, wGap), 0)
             pass
         else: 
             # Zapełnienie X
             pass
     
 if __name__ == "__main__":
-    window = pygame.display.set_mode((600,660))
+    window = pygame.display.set_mode((600,600))
     pygame.display.set_caption("NonogramSolver")
     nonogramBoard = Grid(3, 3, 600, 600, window)
     while True:
         nonogramBoard.refresh()
+        pygame.display.update()
     pygame.quit()
